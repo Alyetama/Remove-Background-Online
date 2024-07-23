da085cdc5c7a46e9f81fc3bbb6494c349a9b46b5
tanhaien <tuananh.ai.en@gmail.com>
2024-07-23 10:49:33 +0700

    mkdir for model before download

--- download_model.py
+++ download_model.py
@@ -2,5 +2,16 @@
 
 from pathlib import Path
 from urllib import request
-model_url = 'https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2net_human_seg.onnx'  # noqa
-request.urlretrieve(model_url, '/.u2net/u2net.onnx')
+
+# Đường dẫn đến thư mục và tệp mô hình
+model_dir = Path('/.u2net')
+model_path = model_dir / 'u2net.onnx'
+
+# Tạo thư mục nếu chưa tồn tại
+model_dir.mkdir(parents=True, exist_ok=True)
+
+# URL của mô hình
+model_url = 'https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2net_human_seg.onnx'
+
+# Tải xuống mô hình
+request.urlretrieve(model_url, model_path)
