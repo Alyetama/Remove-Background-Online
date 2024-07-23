3796373fdac59213c8c0f5c14241d1aa3fb81310
tanhaien <tuananh.ai.en@gmail.com>
2024-07-23 09:01:59 +0700

    replaced u2net.onnx by u2net_human_seg.onnx

--- download_model.py
+++ download_model.py
@@ -3,5 +3,6 @@
 from pathlib import Path
 from urllib import request
 
-model_url = 'https://github.com/Alyetama/Rembg-Online/releases/download/v0.0.0/u2net.onnx'  # noqa
+model_url = 'https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2net_human_seg.onnx'
+#model_url = 'https://github.com/Alyetama/Rembg-Online/releases/download/v0.0.0/u2net.onnx'  # noqa
 request.urlretrieve(model_url, '/.u2net/u2net.onnx')
